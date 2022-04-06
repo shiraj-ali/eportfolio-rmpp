dcecdc31c712d160af510b051ea7310e62af8820
Shiraj <shiraj.ali@gmail.com>
2022-04-06 15:48:02 +0100

    first install

---
+++ js/blog-prime-assets-lib-twp-js-ajax.js
@@ -0,0 +1,52 @@
+jQuery(document).ready( function($) {
+    
+    "use strict";
+
+    var ajaxurl = blog_prime_ajax.ajax_url;
+    var loading = blog_prime_ajax.loading;
+    var loadmore = blog_prime_ajax.loadmore;
+    var nomore = blog_prime_ajax.nomore;
+
+    var page = 2;
+    $('body').on('click', '.loadmore', function() {
+
+        $(this).addClass('loading');
+        $(this).html('<span class="ajax-loader"></span><span>'+loading+'</span>');
+        var data = {
+            'action': 'blog_prime_recommended_posts',
+            'page': page,
+        };
+ 
+        $.post(ajaxurl, data, function(response) {
+            $('.recommended-post-wraper').append(response);
+
+            if( $('body').hasClass('booster-extension') ){
+                likedislike('after-load-'+page);
+            }
+
+            page++;
+
+            if( !$.trim(response) ){
+                $('.loadmore').addClass('no-more-post');
+                $('.loadmore').html(nomore);
+            }else{
+                $('.loadmore').html(loadmore);
+            }
+
+            $('.loadmore').removeClass('loading');
+            
+
+            var pageSection = $(".data-bg");
+            pageSection.each( function () {
+                if ( $(this).attr("data-background")) {
+                    $(this).css("background-image", "url(" + $(this).data("background") + ")");
+                    $('.recommended-load').matchHeight();
+                }
+            });
+
+        });
+
+    });
+
+});
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
